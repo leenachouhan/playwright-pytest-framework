7d2173ab57810396e8e223f603959e6037755004
Leena Chouhan <leenachouhan@GS-6218.local>
2026-05-11 16:30:17 +0530

    updated files with markers and json files

--- tests/test_dashboardfunctionality.py
+++ tests/test_dashboardfunctionality.py
@@ -1,30 +1,40 @@
 from pages.dashboard import Dashboard
 from playwright.sync_api import Page
 import pytest
+import json
 
 dashboardObj = Dashboard()
 
+with open("testdata/dashboard.json") as f:
+    test_data = json.load(f)
+
+@pytest.mark.signout
 def test_signoutfunctionality(login,page:Page):
     logout = dashboardObj.signout(page)
-    assert logout == "Logout Successfully"
+    assert logout == test_data['logout']
 
+@pytest.mark.cart
 def test_emptycartverification(login,page:Page):
     dashboardObj.carts(page)
-    assert page.locator("div.ng-star-inserted>h1").inner_text().strip() == "No Products in Your Cart !"
+    assert page.locator("div.ng-star-inserted>h1").inner_text().strip() == test_data['emptycart']
 
+@pytest.mark.cart
 def test_cartwithitems(login,page:Page):
     # page.pause()
     dashboardObj.carts(page)
     assert page.locator("ul.cartWrap").count() > 0
 
+@pytest.mark.item
 def test_additem(login,page:Page):
-    productadded = dashboardObj.additemtocart(page,"ADIDAS ORIGINAL")
-    assert productadded == "Product Added To Cart"
+    productadded = dashboardObj.additemtocart(page,test_data['item'])
+    assert productadded == test_data['productadded']
 
+@pytest.mark.item
 def test_searchitem(login,page:Page):
-    items = dashboardObj.searchfunctionlity(page,"ZARA")
+    items = dashboardObj.searchfunctionlity(page,test_data['item2'])
     assert items > 0
 
+@pytest.mark.view
 def test_view_functionality(login,page:Page):
-    item = dashboardObj.viewbutton(page,"ADIDAS ORIGINAL")
-    assert item == "ADIDAS ORIGINAL"
+    item = dashboardObj.viewbutton(page,test_data['item'])
+    assert item == test_data['item']
--- tests/test_enrollverification.py
+++ tests/test_enrollverification.py
@@ -11,11 +11,13 @@ with open("credentials.json") as f:
     test_data = json.load(f)
     test_data_list = test_data['user_credentials']
 
+with open("testdata/enrollverification.json") as f:
+    signupdata = json.load(f)
 
+@pytest.mark.signup
 def test_verifyregisterfunctionality(page:Page):
-    
-    text = registerObj.register(page,"Leena","Chouhan","leenaA@gmail.com","1234567890","Engineer","Female","Leena@123","Leena@123")
-    assert text == "Account Created Successfully"
+    text = registerObj.register(page,signupdata['firstname'],signupdata['lastname'],signupdata['email'],signupdata['phonenumber'],signupdata['occupation'],signupdata['gender'],signupdata['password'],signupdata['confirmpassword'])
+    assert text == signupdata['accountcreationmessage']
 
 @pytest.mark.parametrize('testing_data',test_data_list)
 def test_loginfunctionality(page:Page,testing_data):
